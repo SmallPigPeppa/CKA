793b966bb22bc78f24269c414a2ce0274c6fde83
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-24 14:59:42 +0800

    initial

--- multi_scale_cka_resnet50_ms_b2.py
+++ multi_scale_cka_resnet50_ms_b2.py
@@ -60,42 +60,58 @@ class BaselineNet(nn.Module):
         return y1, y2, y3
 
 
+# def forward_features(model, x):
+#     _b = x.shape[0]
+#
+#     # Initial layers
+#     x0 = model.conv1(x)
+#     x0 = model.bn1(x0)
+#     x0 = model.relu(x0)
+#     x0 = model.maxpool(x0)
+#
+#     # ResNet50 layers
+#     x1_0 = model.layer1[0](x0)
+#     x1_1 = model.layer1[1](x1_0)
+#     x1_2 = model.layer1[2](x1_1)
+#
+#     x2_0 = model.layer2[0](x1_2)
+#     x2_1 = model.layer2[1](x2_0)
+#     x2_2 = model.layer2[2](x2_1)
+#     x2_3 = model.layer2[3](x2_2)
+#
+#     x3_0 = model.layer3[0](x2_3)
+#     x3_1 = model.layer3[1](x3_0)
+#     x3_2 = model.layer3[2](x3_1)
+#     x3_3 = model.layer3[3](x3_2)
+#     x3_4 = model.layer3[4](x3_3)
+#     x3_5 = model.layer3[5](x3_4)
+#
+#     x4_0 = model.layer4[0](x3_5)
+#     x4_1 = model.layer4[1](x4_0)
+#     x4_2 = model.layer4[2](x4_1)
+#
+#     return [x0.view(_b, -1), x1_0.view(_b, -1), x1_1.view(_b, -1), x1_2.view(_b, -1),
+#             x2_0.view(_b, -1), x2_1.view(_b, -1), x2_2.view(_b, -1), x2_3.view(_b, -1),
+#             x3_0.view(_b, -1), x3_1.view(_b, -1), x3_2.view(_b, -1), x3_3.view(_b, -1), x3_4.view(_b, -1), x3_5.view(_b, -1),
+#             x4_0.view(_b, -1), x4_1.view(_b, -1), x4_2.view(_b, -1)]
+
 def forward_features(model, x):
     _b = x.shape[0]
+    features = []
 
-    # Initial layers
-    x0 = model.conv1(x)
-    x0 = model.bn1(x0)
-    x0 = model.relu(x0)
-    x0 = model.maxpool(x0)
+    x = model.conv1(x)
+    x = model.bn1(x)
+    x = model.relu(x)
+    features.append(x.view(_b, -1))
 
-    # ResNet50 layers
-    x1_0 = model.layer1[0](x0)
-    x1_1 = model.layer1[1](x1_0)
-    x1_2 = model.layer1[2](x1_1)
+    x = model.maxpool(x)
 
-    x2_0 = model.layer2[0](x1_2)
-    x2_1 = model.layer2[1](x2_0)
-    x2_2 = model.layer2[2](x2_1)
-    x2_3 = model.layer2[3](x2_2)
+    for layer in [model.layer1, model.layer2, model.layer3, model.layer4]:
+        for block in layer:
+            x = block(x)
+            features.append(x.view(_b, -1))
 
-    x3_0 = model.layer3[0](x2_3)
-    x3_1 = model.layer3[1](x3_0)
-    x3_2 = model.layer3[2](x3_1)
-    x3_3 = model.layer3[3](x3_2)
-    x3_4 = model.layer3[4](x3_3)
-    x3_5 = model.layer3[5](x3_4)
-
-    x4_0 = model.layer4[0](x3_5)
-    x4_1 = model.layer4[1](x4_0)
-    x4_2 = model.layer4[2](x4_1)
-
-    return [x0.view(_b, -1), x1_0.view(_b, -1), x1_1.view(_b, -1), x1_2.view(_b, -1),
-            x2_0.view(_b, -1), x2_1.view(_b, -1), x2_2.view(_b, -1), x2_3.view(_b, -1),
-            x3_0.view(_b, -1), x3_1.view(_b, -1), x3_2.view(_b, -1), x3_3.view(_b, -1), x3_4.view(_b, -1), x3_5.view(_b, -1),
-            x4_0.view(_b, -1), x4_1.view(_b, -1), x4_2.view(_b, -1)]
-
-
+    return features
 
 class MSNetPL(pl.LightningModule):
     def __init__(self, args):
