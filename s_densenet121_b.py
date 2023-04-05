b2af593e5b9d87d5b16fbdf295b9da53c90e659b
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-04-05 12:03:32 +0800

    initial

--- s_densenet121_b.py
+++ s_densenet121_b.py
@@ -66,7 +66,7 @@ def forward_features(model, x):
     x = features.transition3(x3)
     x4 = features.denseblock4(x)
 
-    return x1.view(_b, -1), x2.view(_b, -1), x3.view(_b, -1), x4.view(_b, -1)
+    return x.view(_b, -1), x1.view(_b, -1), x2.view(_b, -1), x3.view(_b, -1), x4.view(_b, -1)
 
 
 class MSNetPL(pl.LightningModule):
@@ -95,7 +95,7 @@ def main():
     batch_size = 128
     dataset_size = 128
     num_sweep = 1
-    num_features = 4
+    num_features = 5
     small_size = 32
     large_size = 224
 
