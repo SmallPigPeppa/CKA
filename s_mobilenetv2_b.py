3da42174b0a6137fe3a786cc409ef591e285048f
lwz <905328134@qq.com>
2023-04-06 13:49:26 +0800

    initial

--- s_mobilenetv2_b.py
+++ s_mobilenetv2_b.py
@@ -55,13 +55,15 @@ def forward_features(model, x):
     #     out.append(features[:i](x))
     # return out[0],out[1],out[2],out[3],out[4]
     # Get intermediate features after specific layers
-    x1 = features[:2](x)  # After first ReLU6
+    x1 = features[:3](x)  # After first ReLU6
     x2 = features[:4](x)  # After second InvertedResidual
     x3 = features[:7](x)  # After fourth InvertedResidual
     x4 = features[:14](x)  # After seventh InvertedResidual
-    x5 = features[:18](x)  # After seventh InvertedResidual
+    x5 = features[:16](x)  # After seventh InvertedResidual
+    x6 = features[:18](x)  # After seventh InvertedResidual
+    x7 = features[:19](x)  # After seventh InvertedResidual
 
-    return x1.view(_b, -1), x2.view(_b, -1), x3.view(_b, -1), x4.view(_b, -1), x5.view(_b, -1)
+    return x1.view(_b, -1), x2.view(_b, -1), x3.view(_b, -1), x4.view(_b, -1), x5.view(_b, -1),x6.view(_b, -1), x7.view(_b, -1)
 
 
 class MSNetPL(pl.LightningModule):
@@ -90,7 +92,7 @@ def main():
     batch_size = 128
     dataset_size = 128
     num_sweep = 1
-    num_features = 5
+    num_features = 7
     small_size = 32
     large_size = 224
 
